bb188b9a9f5c34d1d45e24e8dd71e571df6aef70
erika jonell <erika.jonell@gmail.com>
2018-02-19 09:18:30 -0500

    issued actions are now promises

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,12 @@
 
 function create_store(starting_state={}){
-    window.store = new Store(starting_state);
-    return window.store;
+    window.simple_state_store = new Store(starting_state);
+    return window.simple_state_store;
 }
 
 function get_store(){
-    if(window.store){
-        return window.store;
+    if(window.simple_state_store){
+        return window.simple_state_store;
     }else{
         throw new Error('No Store Exists!');
     }
@@ -29,12 +29,27 @@ class Store{
     }
 
     issue_action(action, data){
-        this._update_state(data);
-        if(this.listeners.hasOwnProperty(action)){
-            this.listeners[action].forEach(callback=>{
-                callback(this.state, action);
-            });
-        }
+        let update_state = this._update_state.bind(this);
+            new Promise((resolve,reject)=>{
+            resolve(update_state(data));
+        })
+        .then(store=>{
+            if(store.listeners.hasOwnProperty(action)){
+                store.listeners[action].forEach(callback=>{
+                    callback(store.state, action);
+                });
+            }
+        }).catch(error=>{
+            console.log('something broke during action issue:',error);
+        });
+        
+        //OLD WAY
+        // this._update_state(data);
+        // if(this.listeners.hasOwnProperty(action)){
+        //     this.listeners[action].forEach(callback=>{
+        //         callback(this.state, action);
+        //     });
+        // }
     }
 
     unsubscribe(){
@@ -50,6 +65,7 @@ class Store{
         for(let key of keys){
             this.state[key] = update[key];
         }
+        return this;
     }
 }
 
